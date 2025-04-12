4f732f91b05799b6a3f3c875e61fa7250dd0cd89
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2025-04-12 00:51:09 -0300

    chore: add Docker and Prisma configuration files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import Fastify from "fastify";
 import { application } from "./app";
 
-const fastify = Fastify({logger: true});
+const fastify = Fastify({ logger: true });
 
 const start = async () => {
   fastify.register(application);
-  await fastify.listen({port: 3000});
+  await fastify.listen({ port: 3000, host: "0.0.0.0" });
 };
 
 start();
