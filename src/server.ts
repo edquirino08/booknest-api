ba46715eea43b91ee11e32a507b762e0750cb08c
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2025-04-14 08:53:25 -0300

    feat: protecting resources with FastifyJwt

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
 import { application } from "./app";
+import { setupJwt } from "./infra/http/security.configuration";
+import dotenv from "dotenv";
 
 const fastify = Fastify({ logger: true });
+dotenv.config();
 
 const start = async () => {
+  setupJwt(fastify);
   fastify.register(application);
   await fastify.listen({ port: 3000, host: "0.0.0.0" });
 };
